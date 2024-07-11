da6850083f0f4d313462a59623783e7ee8a68ad4
Lucas <lumuryllo86@gmail.com>
2024-07-11 14:38:07 -0300

    feat: created pages

--- src/App.js
+++ src/App.js
@@ -1,24 +1,30 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import { Routes, Route } from 'react-router-dom';
+
+import GlobalStyles from './styles/globalStyles';
+import Header from './components/header';
+import Sidebar from './components/sidebar';
+import RendaFixa from './pages/FundoRendaFixa';
+import Page404 from './pages/page404';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <Routes>
+        <Route path="/" element={<Header />}>
+          <Route path="minha_carteira" element={<Sidebar />}>
+            <Route path="classe_ativos" element={null} />
+            <Route
+              path="classe_ativos/fundo_renda_fixa"
+              element={<RendaFixa />}
+            />
+            <Route path="*" element={<Page404 />} />
+          </Route>
+          <Route path="*" element={<Page404 />} />
+        </Route>
+      </Routes>
+      <GlobalStyles />
+    </>
   );
 }
 
